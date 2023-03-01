e4b3b231e1a02942eb09679cb8c35b51d66eb3c4
Fredrick Ocheing <fredrick.oluoch@calkenya.com>
2023-03-01 20:05:23 +0300

    Initial commit

---
+++ functions/index.js
@@ -0,0 +1,52 @@
+const admin = require('firebase-admin');
+const functions = require('firebase-functions');
+
+admin.initializeApp();
+
+/**
+ * This function uses the functions.https.onRequest trigger to listen for incoming HTTP requests. . 
+ * When a request is received, it saves it into firebase database
+ */
+exports.mpesaStkCallback = functions.https.onRequest((req, res) => {
+    const data = req.body;
+    functions.logger.info("Received Data: ", data, { structuredData: true });
+
+    const merchantRequestID = data.Body.stkCallback.MerchantRequestID
+    const resultCode = data.Body.stkCallback.ResultCode
+
+    const db = admin.database();
+    const mpesaRef = db.ref(`mpesa/stk/${merchantRequestID}`);
+
+    const payload = data.Body.stkCallback
+
+    if (resultCode !== 0) {
+        payload.Status = "FAILED"
+    } else {
+        payload.Status = "SUCCESS"
+
+        //Flatten callback metadata into an Object
+        const metaData = payload.CallbackMetadata.Item.reduce((acc, cur) => {
+            if (cur.Value) {
+                acc[cur.Name] = cur.Value;
+            } else {
+                acc[cur.Name] = null;
+            }
+            return acc;
+        }, {});
+
+        Object.assign(payload, metaData)
+
+        //remove callback metadata from payload
+        delete payload.CallbackMetadata
+    }
+
+    //Update Mpesa Transaction
+    mpesaRef.update(payload).then(() => {
+        console.log('Transaction saved to Realtime Database:', data);
+        res.sendStatus(200);
+    }).catch(error => {
+        console.error('Realtime Database error:', error);
+        res.sendStatus(500);
+    });
+});
+
